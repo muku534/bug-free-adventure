9948cf89e1250c3aaf8c1cd2e92daf14b9077b1d
Prajapati Mukesh <prajapatimukesh0111@gmail.com>
2022-12-28 13:43:04 +0530

    updated userSchema

--- backend/model/UserDetails.js
+++ backend/model/UserDetails.js
@@ -1,15 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const UserDetailsScehma = new mongoose.Schema(
-{
-    fname:String,
-    lname:String,
-    email:String,
-    phoneNo:String, // New content
-},
-{
-    collection:"Buyers",
-}
-);
+const UserDetailsScehma = new mongoose.Schema({
 
-mongoose.model("Buyers",UserDetailsScehma);
+    fname: {
+        type:String,
+        required:true
+    },
+    lname: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true
+    },
+    password: {
+        type:String,
+        required:true
+    }
+})
+
+const User = mongoose.model('Buyer',UserDetailsScehma);
+
+module.exports = User;
